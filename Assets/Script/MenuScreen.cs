2f2e9d44e91c31adc2e33354a5e1dd8a2a7a99ea
skitak <bas2205@live.fr>
2018-06-19 10:37:30 +0200

    first scene added, players facing with anims, first menu

---
+++ Assets/Script/MenuScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreen : MonoBehaviour {
+
+
+	public GameObject startMenu;
+	private GameObject actualMenu, previousMenu;
+	
+
+	void Start() {
+		actualMenu = startMenu;	
+	}
+	
+	public void swapToMenu(GameObject newMenu){
+		
+	}
+}
