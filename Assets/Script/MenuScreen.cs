f5bdda87df778263614752eb409538fd825c0091
skitak <bas2205@live.fr>
2018-06-19 12:28:28 +0200

    Menu script et menu

--- Assets/Script/MenuScreen.cs
+++ Assets/Script/MenuScreen.cs
@@ -7,13 +7,12 @@ public class MenuScreen : MonoBehaviour {
 
 	public GameObject startMenu;
 	private GameObject actualMenu, previousMenu;
-	
+	Rigidbody rb;
 
 	void Start() {
 		actualMenu = startMenu;	
 	}
-	
+
 	public void swapToMenu(GameObject newMenu){
-		
 	}
 }
