349e23eb9f848f69dedc1a482db207e83ff2f36b
JARTRON\Skaje <sorignet.gautrot.arthur@gmail.com>
2018-08-16 10:01:08 +0200

    First VFX
    Signed-off-by: JARTRON\Skaje <sorignet.gautrot.arthur@gmail.com>

---
+++ Assets/VFX/BackGroundFlares/BackgroundFlareManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFlareManager : MonoBehaviour {
+
+public ParticleSystem BackgroundFlareSystem;
+public int ParticlesViolence;
+
+
+
+void Update ()
+{
+	Violence ();
+}
+
+void Violence ()
+{
+var main = BackgroundFlareSystem.main;
+
+	//max Particules Number
+	if (main.maxParticles < 100)
+		main.maxParticles = (ParticlesViolence * 2);
+
+	//Particles Spawn Number
+	if (ParticlesViolence > 0)
+		BackgroundFlareSystem.emissionRate = Mathf.RoundToInt( Mathf.Log (ParticlesViolence));
+}
+
+
+}
